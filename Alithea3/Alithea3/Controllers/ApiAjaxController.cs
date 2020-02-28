8696bf9b877cc8d812055cc866ca10c0d2ca9063
Bằng Nguyễn <you@example.com>
2020-02-28 20:17:14 +0700

    dataTable jquery

---
+++ Alithea3/Alithea3/Controllers/ApiAjaxController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Alithea3.Controllers.Service.OrderManager;
+using Alithea3.Models;
+using Alithea3.Models.ViewModel;
+
+namespace Alithea3.Controllers.Api
+{
+    public class ApiAjaxController : Controller
+    {
+        private MyDbContext db = new MyDbContext();
+        private OrderService _orderService = new OrderService();
+
+        public ActionResult GetListOrder(int? page, int? limit, string start, string end)
+        {
+//            var hashtable = _orderService.listPagination(page, limit, start, end);
+//            var data = hashtable["listOrder"] as List<Order>;
+
+            var orders = db.Orders.Select(o => new OrdersViewModel
+            {
+                OrderID = o.OrderID,
+                RoleNumber = o.RoleNumber,
+                OrderDate = o.OrderDate,
+                Quantity = o.Quantity,
+                TotalPrice = o.TotalPrice,
+                UserID = o.UserID,
+                FullName = o.FullName,
+                Email = o.Email,
+                Phone = o.Phone,
+                Address = o.Address,
+                Commnet = o.Commnet,
+                ShippedDate = o.ShippedDate,
+                Status = o.Status,
+                RequireDate = o.RequireDate
+            }).ToList();
+//            var jsonResult = new JsonResult() { JsonRequestBehavior  = JsonRequestBehavior.AllowGet};
+//            jsonResult.Data = new
+//            {
+//                success = true,
+//                data = orders
+//            };
+
+            return Json(new {data = orders}, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Alithea3/Alithea3/Models/ViewModel/OrdersViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Alithea3.Models.ViewModel
+{
+    public class OrdersViewModel
+    {
+        public int OrderID { get; set; }
+
+        [DisplayName("Mã đặt hàng")]
+        public string RoleNumber { get; set; }
+
+        [DisplayName("Ngày đặt hàng")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime OrderDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime? RequireDate { get; set; }
+
+        [DisplayName("Ngày giao hàng")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime? ShippedDate { get; set; }
+
+        [DisplayName("Tổng số lượng")]
+        public int Quantity { get; set; }
+
+        [DisplayName("Tổng đơn hàng")]
+        [DataType(DataType.Currency)]
+        public double TotalPrice { get; set; }
+
+        [DisplayName("Chú thích")]
+        public string Commnet { get; set; }
+
+        [ForeignKey("UserAccount")]
+        public int? UserID { get; set; }
+
+        [DisplayName("Họ tên")]
+        public string FullName { get; set; }
+
+        [DisplayName("Địa chỉ")]
+        public string Address { get; set; }
+
+        [DisplayName("Số điện thoại")]
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        [DisplayName("Trạng thái")]
+        public Order.StatusOrder Status { get; set; }
+    }
+}
